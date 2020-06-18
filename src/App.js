af3a7ba4e30007642bde5e1f8b79a96f30b982e8
CerenYal <c.yaliniz@hotmail.de>
2020-06-18 13:55:51 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h3>WELCOME</h3>
         </a>
       </header>
     </div>
